5e56d9eda29ec0b8491fbcee9a38c035ad9b1663
amokarev <mokarev.a@gmail.com>
2021-04-23 14:03:58 +0300

    first commit

---
+++ src/main/java/ru/mokarev/ipldashboard/IplDashboardApplication.java
@@ -0,0 +1,13 @@
+package ru.mokarev.ipldashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplDashboardApplication.class, args);
+	}
+
+}
