5dbe185a83e3b9ef9fbbb0e038de0c617dc39862
amokarev <mokarev.a@gmail.com>
2021-05-07 10:34:27 +0300

    Add React components: TeamPage.js MatchDetailCard.js MatchSmallCard.js

--- src/main/java/ru/mokarev/ipldashboard/controller/TeamController.java
+++ src/main/java/ru/mokarev/ipldashboard/controller/TeamController.java
@@ -1,6 +1,7 @@
 package ru.mokarev.ipldashboard.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import ru.mokarev.ipldashboard.repository.MatchRepository;
 import ru.mokarev.ipldashboard.repository.TeamRepository;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private final TeamRepository teamRepository;
